76b2fc7a3a394ef4edaf9438815cd7573e6addf6
Precious Diala <dialpressy@gmail.com>
2022-09-19 23:17:35 +0000

    header file for the C program

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -1,5 +1,6 @@
 #ifndef LISTS_H
 #define LISTS_H
+
 #include <stdlib.h>
 
 /**
@@ -8,12 +9,12 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * for ALX project
+ * for Holberton project
  */
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
 
 size_t print_listint(const listint_t *h);
