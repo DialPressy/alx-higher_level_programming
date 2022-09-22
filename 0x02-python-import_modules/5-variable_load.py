5de92dd4813c1bde5a9e4e6ee46b0b78b0cc14f0
Precious Diala <dialpressy@gmail.com>
2022-09-22 21:49:25 +0000

    Python program that imports the variable a from the file variable_load_5.py and prints its value

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from hidden_4 import *
-    arr = dir()
-    for i in range(0, len(arr)):
-        if arr[i][0:2] != "__":
-            print("{}".format(arr[i]))
+    from variable_load_5 import a
+    print("{}".format(a))
