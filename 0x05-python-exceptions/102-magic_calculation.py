3488eaadc4f84dcb53ee30a3fb4b38b2e2506e77
Precious Diala <dialpressy@gmail.com>
2022-10-11 02:50:18 +0000

    Python function found from its ByteCode (advanced)

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
-def magic_calculation(a, b):
-    result = 0
-    for i in range(1, 3):
-        try:
-            if i > a:
-                raise Exception('Too far')
 
-                result += a ** b / i
-        except:
-            result = b + a
-            break
-    return result
+def magic_calculation(a, b):
+	result = 0
+
+	for i in range(1, 3):
+	   try:
+	       if i > a:
+		  raise Exception('Too far')
+
+	       result += a ** b / i
+	   except:
+	       result = b + a
+	       break
+
+	return result
